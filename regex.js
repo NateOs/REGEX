dfe3a5f1881d802aee72f9a891c31f74c83e8060
nateos <sodjanathan@gmail.com>
2021-10-14 08:49:30 +0000

    Init

---
+++ regex.js
@@ -0,0 +1,10 @@
+// Regex is used in programming to match patterns in strings
+
+let sentence = "The dog chased the cat.";
+let regex = /the/
+
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // This means check if 'myRegex' matches anything in 'myString'
+
+console.log(result);
