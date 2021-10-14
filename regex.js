80df6a22a71f9dff123d96f75b204799b0601b4a
nateos <sodjanathan@gmail.com>
2021-10-14 10:31:36 +0000

    ignoring case with the 'i' flag

--- regex.js
+++ regex.js
@@ -3,7 +3,7 @@
 let sentence = "The dog chased the cat.";
 let regex = /the/
 
-let myString = "Hello, World!";
+// let myString = "Hello, World!";
 let myRegex = /Hello/;
 // let result = myRegex.test(myString); //* This means check if 'myRegex' matches anything in 'myString'
 
@@ -20,9 +20,16 @@ let waldoRegex = /Waldo/;
 //! Match a literal string with different possibilities
 let petString = "James has  a pet cat.";
 let petRegex = /dog|cat|bird|fish/;
-let result = petRegex.test(petString);
+// let result = petRegex.test(petString);
 
 //* returns true because 'cat' is a match, will also return true if dog
 //* was a match or any of the other strings in the regex with the '|' sign.
-console.log(result); 
+// console.log(result);
+
+//! Using the 'i' flag to ignore case 
+let myString = "freeCodeCamp";
+let fccRegex = /freecodecamp/i;
+let result = fccRegex.test(myString);
+console.log(result);
+
 
